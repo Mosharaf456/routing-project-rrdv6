8d7cae747803d0bb1aba10ef92dd61e7e69a435b
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-22 16:13:44 +0600

    ref oulet added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+// import { Suspense, lazy } from 'react';
+
 import Login from './components/Login';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import NewBook from './components/NewBook';
-import { lazy } from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import NotFound from './components/NotFound';
+import BookLayout from './components/BookLayout';
 
-const Home = lazy(() => import('./components/Home'));
+// const Home = lazy(() => import('./components/Home'));
+import Home from './components/Home';
+
+
 function App() {
   return (
     <>
@@ -25,11 +30,22 @@ function App() {
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
+
+        <Route path='/books' element={<BookLayout />}>
+          <Route index element={<BookList />} />
+          <Route path=':bookId' element={<Book />} />
+          <Route path='new' element={<NewBook />} />
+        </Route>
+
         <Route path='/login' element={<Login />} />
+
         <Route path='/home' element={<Home />} />
-        <Route path='/books' element={<BookList />} />
+ 
+          
+
+        {/* <Route path='/books' element={<BookList />} />
         <Route path='/books/:bookId' element={<Book />} />
-        <Route path='/books/new' element={<NewBook />} />
+        <Route path='/books/new' element={<NewBook />} /> */}
 
         <Route path='*' element={<NotFound />} />
       </Routes>
---
+++ src/components/BookLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
+
+function BookLayout() {
+    return (
+      <>
+        <Link to='/books/1'>Book 1</Link>
+        <br />
+        <br />
+        <Link to='/books/2'>Book 2</Link>
+        <br /> 
+        <Link to='/books/new'>New Book</Link>
+        
+        <Outlet />
+      </>
+    )
+}
+
+export default BookLayout
